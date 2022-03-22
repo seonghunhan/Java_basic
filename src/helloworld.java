5b892cdf6836044c8f067cd955f27820d1a4319c
seonghun.han <hsh931212a@naver.com>
2022-03-22 14:31:30 +0900

    java_basic

---
+++ src/helloworld.java
@@ -0,0 +1,15 @@
+public class helloworld {
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[5]; // 배열 선언 단계
+        arr1[0] = 100;
+        arr1[1] = 200;
+        arr1[2] = 300;
+        arr1[3] = 400;
+        arr1[4] = 500;
+
+        System.out.println("arr[0] : " + arr1[0]);
+        System.out.println("arr[1] : " + arr1[1]);
+        System.out.println("arr[2] : " + arr1[2]);
+    }
+}
